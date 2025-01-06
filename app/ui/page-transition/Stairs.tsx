de2c0c1e5dcc9c564b5c2e6683c9089e87f11332
yvncv <146147151+yvncv@users.noreply.github.com>
2025-01-05 22:33:22 -0500

    page transition animation

---
+++ app/ui/page-transition/Stairs.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const stairAnimation = {
+    initial: { bottom: "0%" },
+    animate: { bottom: "100%" },
+    exit: { bottom: ["100%", "0%"] },
+};
+
+const reverseIndex = (index: number) => 6 - index - 1;
+
+const Stairs = () => {
+    return (
+        <>
+            {[...Array(6)].map((_, index) => {
+                return (
+                    <motion.div
+                        key={index}
+                        className="h-full w-full bg-rojo relative"
+                        variants={stairAnimation}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        transition={{
+                            duration: 0.4,
+                            ease: "easeInOut",
+                            delay: reverseIndex(index) * 0.1,
+                        }}
+                    />
+                )
+            })}
+        </>
+    );
+};
+
+export default Stairs;
