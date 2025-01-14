44784aa4da0896e70e4d4716ae38dd5b17d74596
yvncv <146147151+yvncv@users.noreply.github.com>
2025-01-14 13:02:13 -0500

    admin sidebar

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -23,14 +23,12 @@ const Page = () => {
 
   return (
     <div className="flex h-screen">
-      {/* Sidebar */}
       <AdminSideBar setActiveComponent={setActiveComponent} />
-
-      {/* Main content */}
-      <div className="flex-1 p-6 bg-gray-100 overflow-y-auto ml-64">
+      <div className="flex-1 p-6 bg-gray-100 overflow-y-auto md:ml-64">
         {renderActiveComponent()}
       </div>
     </div>
+
   );
 };
 
