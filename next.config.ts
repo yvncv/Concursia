7f54c74acc674a8cbe6715b3d3698d31f75a455a
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-02-14 17:14:14 -0500

    fix: Invalid src prop

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
         hostname: "perusumaq.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/**",
+      },
     ],
   },
 };
