d300b5a7f184112e37c06d0431cbaef817def3d9
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-02-21 06:18:42 -0500

    feat: filtrar tickets v1

--- app/organizer/events/[id]/sections/Tickets.tsx
+++ app/organizer/events/[id]/sections/Tickets.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CustomEvent } from '@/app/types/eventType';
 import useTicket from '@/app/hooks/useTicket';
 import useUsers from '@/app/hooks/useUsers';
 import { Ticket } from '@/app/types/ticketType';
 import { User } from '@/app/types/userType';
+import {CircleX} from "lucide-react";
 
 interface TicketsProps {
     event: CustomEvent;
@@ -16,15 +17,21 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [users, setUsers] = useState<User[]>([]);
 
+    const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false);
+    const [filteredTickets, setFilteredTickets] = useState<Ticket[]>([]);
+
+    useEffect(() => {
+        setFilteredTickets(tickets);
+    }, [tickets]);
+
     const openModal = async (ticket: Ticket) => {
         setSelectedTicket(ticket);
         setIsModalOpen(true);
 
-        if(ticket.level === 'Individual'|| ticket.level === 'Seriado'){
+        if(ticket.level === 'Individual' || ticket.level === 'Seriado'){
             const user1 = await getUserById(ticket.usersId[0]);
             setUsers([user1].filter(Boolean) as User[]);
-
-        }else {
+        } else {
             const user1 = await getUserById(ticket.usersId[0]);
             const user2 = await getUserById(ticket.usersId[1]);
             setUsers([user1, user2].filter(Boolean) as User[]);
@@ -37,22 +44,75 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
         setUsers([]);
     };
 
-    if (loading) {
-        return <div className="p-6">Loading...</div>;
-    }
+    const toggleFilterMenu = () => {
+        setIsFilterMenuOpen(!isFilterMenuOpen);
+    };
 
-    if (error) {
-        return <div className="p-6 text-red-500">{error}</div>;
-    }
+    const filterTickets = (filter:string ,criteria: string) => {
+        if (criteria === 'Todos') {
+            setFilteredTickets(tickets);
+        } else {
+            if(filter === 'levels'){
+                const filtered = tickets.filter(ticket => ticket.level === criteria);
+                setFilteredTickets(filtered);
+            }
+            if(filter === 'categories'){
+                const filtered = tickets.filter(ticket => ticket.category === criteria);
+                setFilteredTickets(filtered);
+            }
+            if(filter === 'status'){
+                const filtered = tickets.filter(ticket => ticket.status === criteria);
+                setFilteredTickets(filtered);
+            }
+        }
+        setIsFilterMenuOpen(false);
+    };
+
+    if (loading) { return <div className="p-6">Loading...</div>; }
+    if (error) { return <div className="p-6 text-red-500">{error}</div>; }
+
+    const headers: string[] = ["ID", "Modalidad", "Categoría", "Fecha de Registro", "Estado", "Acciones"];
+    const levels: string[] = ["Todos", "Seriado", "Individual", "Novel Novel", "Novel Abierto", "Novel Abierto A", "Novel Abierto B", "Nacional"];
+    const categories: string[] = ["Baby", "Pre-Infante", "Infante", "Infantil", "Junior", "Juvenil", "Adulto", "Senior", "Master", "Oro"];
+    const status: string[] = ["Pendiente", "Cancelado"];
 
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">Tickets del Evento</h1>
+            <button onClick={toggleFilterMenu} className="mb-4 px-4 py-2 bg-blue-500 text-white rounded">Filtrar Tickets</button>
+            {isFilterMenuOpen && (
+                <div className="mb-4 p-4 bg-gray-100 rounded shadow flex">
+                    <div className="flex-1">
+                        {levels.map(level => (
+                            <button onClick={() => filterTickets('levels', level)}
+                                    className="block w-full text-left px-4 py-2" key={level}>
+                                {level}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="flex-1">
+                        {categories.map(category => (
+                            <button onClick={() => filterTickets('categories', category)}
+                                    className="block w-full text-left px-4 py-2" key={category}>
+                                {category}
+                            </button>
+                        ))}
+                    </div>
+                    <div className="flex-1">
+                        {status.map(state => (
+                            <button onClick={() => filterTickets('status', state)}
+                                    className="block w-full text-left px-4 py-2" key={state}>
+                                {state}
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            )}
             <div className="overflow-x-auto rounded-xl">
                 <table className="w-full border-collapse">
                     <thead className="bg-gray-100 dark:bg-gray-700">
                     <tr>
-                        {["ID", "Modalidad", "Categoría", "Fecha de Registro", "Estado", "Acciones"].map((header) => (
+                        {headers.map((header) => (
                             <th
                                 key={header}
                                 className="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider border-b dark:border-gray-600"
@@ -63,7 +123,7 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
                     </tr>
                     </thead>
                     <tbody>
-                    {tickets.map((ticket) => (
+                    {filteredTickets.map((ticket) => (
                         <tr key={ticket.id}>
                             <td className="px-4 py-3 border-b dark:border-gray-600">{ticket.id}</td>
                             <td className="px-4 py-3 border-b dark:border-gray-600">{ticket.level}</td>
@@ -71,9 +131,9 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
                             <td className="px-4 py-3 border-b dark:border-gray-600">{ticket.registrationDate.toDate().toLocaleDateString()}</td>
                             <td className="px-4 py-3 border-b dark:border-gray-600">{ticket.status}</td>
                             <td className="px-4 py-3 border-b dark:border-gray-600">
-                                <button className="text-gray-500 hover:underline" onClick={() => openModal(ticket)}>Detalles</button>
-                                <button className="text-blue-500 hover:underline ml-2">Editar</button>
-                                <button className="text-red-500 hover:underline ml-2">Eliminar</button>
+                                <button className="text-blue-500 hover:underline"
+                                        onClick={() => openModal(ticket)}>Detalles
+                                </button>
                             </td>
                         </tr>
                     ))}
@@ -84,11 +144,19 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
             {isModalOpen && selectedTicket && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
                     <div className="bg-white p-6 rounded-lg shadow-lg">
-                        <h2 className="text-xl font-bold mb-4">Detalles del Ticket</h2>
+                        <div className="flex justify-between mt-4">
+                            <h2 className="text-xl font-bold mb-4">Detalles del Ticket</h2>
+                            <div>
+                                <button onClick={closeModal} className="text-gray-500 hover:text-gray-700">
+                                    <CircleX size={40} className="text-blue-500 hover:text-red-600 transition-colors"/>
+                                </button>
+                            </div>
+                        </div>
+
                         <table className="w-full border-collapse">
                             <tbody>
                             <tr>
-                                <td className="px-4 py-2 font-semibold">ID:</td>
+                            <td className="px-4 py-2 font-semibold">ID:</td>
                                 <td className="px-4 py-2">{selectedTicket.id}</td>
                             </tr>
                             <tr>
@@ -113,7 +181,6 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
                                     <div className="space-y-2">
                                         {users.map(user => (
                                             <div key={user.id}>{user.dni} {user.firstName} {user.lastName}</div>
-
                                         ))}
                                     </div>
                                 </td>
@@ -130,7 +197,13 @@ const Tickets: React.FC<TicketsProps> = ({ event }) => {
                             </tr>
                             </tbody>
                         </table>
-                        <button className="mt-4 text-blue-500 hover:underline" onClick={closeModal}>Cerrar</button>
+                        <div className="flex justify-between mt-4">
+                            <button className="text-blue-500 hover:underline"> </button>
+                            <div>
+                                <button className="bg-green-500 rounded-xl p-4 text-white hover:underline ml-2">Confirmar</button>
+                                <button className="bg-red-500 rounded-xl p-4 text-white hover:underline ml-2">Eliminar</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
