2cdcb529917d837a0e4e7b1b58d1480617cdceb9
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-02-26 15:06:59 -0500

    Feat: Display participants v1.0

--- app/organizer/events/[id]/sections/Participants.tsx
+++ app/organizer/events/[id]/sections/Participants.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CustomEvent } from '@/app/types/eventType';
+import { Participant } from '@/app/types/participantType';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import { db } from '@/app/firebase/config';
 
 interface ParticipantsProps {
     event: CustomEvent;
 }
 
 const Participants: React.FC<ParticipantsProps> = ({ event }) => {
+    const [participants, setParticipants] = useState<Participant[]>([]);
+
+    useEffect(() => {
+        const fetchParticipants = async () => {
+            const q = query(collection(db, 'participants'), where('eventId', '==', event.id));
+            const querySnapshot = await getDocs(q);
+            const participantsList: Participant[] = querySnapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            } as Participant));
+            setParticipants(participantsList);
+        };
+
+        fetchParticipants();
+    }, [event.id]);
+
     return (
         <div className="p-6">
-            <h1 className="text-2xl font-bold mb-4">Paricipantes del Evento</h1>
-            <div className="bg-white p-4 shadow-md rounded-lg">
-                <h2 className="text-lg font-semibold">Detalles del Evento</h2>
-                <p><strong>Nombre:</strong> {event.name}</p>
+            <h1 className="text-2xl font-bold mb-4">Participantes del Evento</h1>
+            <div className="bg-white p-4 shadow-md rounded-lg mt-4">
+                <h2 className="text-lg font-semibold mb-4">Lista de Participantes</h2>
+                {participants.length > 0 ? (
+                    <table className="min-w-full bg-white border border-gray-300 ">
+                        <thead className="bg-gray-100 dark:bg-gray-700">
+                        <tr className="bg-gray-100">
+                            <th className="py-2 px-4 border">Código</th>
+                            <th className="py-2 px-4 border">Categoría</th>
+                            <th className="py-2 px-4 border">Nivel</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {participants.map(participant => (
+                            <tr key={participant.id} className="hover:bg-gray-50">
+                                <td className="border px-4 py-2">{participant.code}</td>
+                                <td className="border px-4 py-2">{participant.category}</td>
+                                <td className="border px-4 py-2">{participant.level}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>No hay participantes registrados para este evento.</p>
+                )}
             </div>
         </div>
     );
