83784f76087811fd3d7948bde7b8cb58c401e5fa
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-02-27 17:56:54 -0500

    Fix: Advanced use Event placeholder

--- app/firebase/config.ts
+++ app/firebase/config.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_appId
+  apiKey: "AIzaSyDrWTuIbOLOomuuUkVl7VHzHaK58isVBSo",
+  authDomain: "next-proj-216fd.firebaseapp.com",
+  projectId: "next-proj-216fd",
+  storageBucket: "next-proj-216fd.firebasestorage.app",
+  messagingSenderId: "465667546272",
+  appId: "1:465667546272:web:50beb0bbf0e39c8a2cfdc5"
 };
 
 // Initialize Firebase
