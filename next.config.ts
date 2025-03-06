058762c11541b3193e8760c14a31aa54591c5c1b
yvncv <146147151+yvncv@users.noreply.github.com>
2025-03-06 18:35:04 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    // unoptimized: true,
     domains: ['firebasestorage.googleapis.com', 'next-proj-216fd.web.app'], // Agrega los dominios permitidos
     remotePatterns: [
       {
