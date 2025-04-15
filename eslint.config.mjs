646e34429a3ae8ab8f3858f2980260f61b7b6e88
yvncv <146147151+yvncv@users.noreply.github.com>
2025-04-14 23:15:52 -0500

    fix: additions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
+    extends: ['next/core-web-vitals', 'next/typescript', 'next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
   }),
 ]
 
