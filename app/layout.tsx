3366aaee3098c7f9e598569b346242228d0865fb
yvncv <146147151+yvncv@users.noreply.github.com>
2025-05-06 14:15:05 -0500

    fix: capturar datos de ubicacion de evento

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import RootLayoutClient from './RootLayoutClient';
+import Script from "next/script";
 
 
 const poppins = localFont({
@@ -21,6 +22,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" translate="yes">
+      <head>
+        <Script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_KEY}&libraries=places`}
+          strategy="beforeInteractive"
+        />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         <RootLayoutClient>{children}</RootLayoutClient>
       </body>
