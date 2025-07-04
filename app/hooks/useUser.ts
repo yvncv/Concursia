397961da3fbf22fb0ef8ba158960eb0daf2b72db
yvncv <146147151+yvncv@users.noreply.github.com>
2025-07-03 21:17:03 -0500

    Update useUser.ts

--- app/hooks/useUser.ts
+++ app/hooks/useUser.ts
@@ -1,28 +1,30 @@
-// useUser.ts
+// app/hooks/useUser.ts
 'use client';
 
 import { useState, useEffect } from 'react';
-import { auth, db } from '../firebase/config';
-import { onAuthStateChanged } from 'firebase/auth';
+import { db } from '@/app/firebase/config';
+import { getAuth, onAuthStateChanged, User as FirebaseUser } from 'firebase/auth';
 import { getDoc, doc } from 'firebase/firestore';
-import { User } from '../types/userType';
+import { User } from '@/app/types/userType';
 
-const useUser = () => {
+export default function useUser() {
   const [user, setUser] = useState<User | null>(null);
   const [loadingUser, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+    // Llamamos a getAuth() en el cliente
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser: FirebaseUser | null) => {
       if (firebaseUser) {
         try {
-          const userDoc = await getDoc(doc(db, "users", firebaseUser.uid));
+          const userDoc = await getDoc(doc(db, 'users', firebaseUser.uid));
           if (userDoc.exists()) {
             setUser({ ...firebaseUser, ...userDoc.data() } as User);
           } else {
             setUser(firebaseUser as User);
           }
         } catch (error) {
-          console.error("Error obteniendo datos de Firestore:", error);
+          console.error('Error obteniendo datos de Firestore:', error);
         }
       } else {
         setUser(null);
@@ -34,6 +36,4 @@ const useUser = () => {
   }, []);
 
   return { user, loadingUser };
-};
-
-export default useUser;
+}
