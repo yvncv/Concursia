e9e3727cd6db83c69c1cb7a9e9c39208b0065f9e
Piero Alonso Flores Munarriz <202110478@urp.edu.pe>
2025-07-19 00:07:31 -0500

    fix: cambio de estructura de categories

---
+++ app/utils/category/determineCategory.ts
@@ -0,0 +1,16 @@
+// utils/determineCategory.ts
+import { AgeCategory, CategoryLevel } from "@/app/types/categoriesType";
+
+export const determineCategory = (
+  birthDate: Date,
+  referenceDate: Date,
+  categories: AgeCategory[]
+): CategoryLevel | undefined => {
+  let age = referenceDate.getFullYear() - birthDate.getFullYear();
+  const monthDiff = referenceDate.getMonth() - birthDate.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && referenceDate.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return categories.find(cat => age >= cat.minAge && age <= cat.maxAge)?.name;
+};
